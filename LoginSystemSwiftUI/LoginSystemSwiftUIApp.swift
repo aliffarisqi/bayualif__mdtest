b9815b41dca7a95b7a24516bff9cd3618b3e65d2
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2024-05-15 07:20:43 +0700

    Initial Commit

---
+++ LoginSystemSwiftUI/LoginSystemSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSystemSwiftUIApp.swift
+//  LoginSystemSwiftUI
+//
+//  Created by Bayu Alif Farisqi on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginSystemSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
